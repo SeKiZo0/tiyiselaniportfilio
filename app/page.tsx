0c9f166a8691a2637ed9119bafd0009191b85932
morris <tmorrismofamadi@gmail.com>
2025-05-31 18:34:39 +0200

    Updated gitlab deployment settings

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold text-blue-900">Coming soon broskies</h1>
+      <h1 className="text-3xl font-bold text-blue-900">Coming soon broskiess</h1>
     </main>
   );
 }
