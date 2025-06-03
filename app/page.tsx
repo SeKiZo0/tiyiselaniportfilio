a199f186b17215874113d582099764b1f62adba9
morris <tmorrismofamadi@gmail.com>
2025-06-03 15:22:39 +0200

    Made changes to the Dockerfile to make sure it builds

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,19 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold text-blue-900">Coming soon broskiess</h1>
+        <section>
+            {/*Build some cool animation for here*/}
+            I’m Morris Tiyiselani Mofamadi, and I build smart software that just works.
+        </section>
+        <section>
+            {/*Project section with a auto scroll wheel*/}
+        </section>
+        <section>
+            {/*Skill section with accordian like thing*/}
+        </section>
+        <section>
+        {/*Contact section*/}
+        </section>
     </main>
   );
 }
